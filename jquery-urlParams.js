ea1e21eeaceae4deb6a884b7372ddd7ae104f724
Mara Kim <hacker.root@gmail.com>
2014-07-19 16:37:29 -0500

    Add jquery-urlParams.js

---
+++ jquery-urlParams.js
@@ -0,0 +1,42 @@
+/*
+   jQuery-urlParams
+    Constructs an object representing the URL parameters
+
+  Copyright (c) 2014 Mara Kim
+
+  Permission is hereby granted, free of charge, to any person obtaining a copy
+  of this software and associated documentation files (the "Software"), to deal
+  in the Software without restriction, including without limitation the rights
+  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+  copies of the Software, and to permit persons to whom the Software is
+  furnished to do so, subject to the following conditions:
+
+  The above copyright notice and this permission notice shall be included in
+  all copies or substantial portions of the Software.
+
+  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+  THE SOFTWARE.
+*/
+
+jQuery.urlParams = (function() {
+    var Param = {};
+    var URLVariables = window.location.search.substring(1).split('&');
+    for (var index in URLVariables) {
+        var Variable = URLVariables[index].split('=');
+        if (Object.keys(Param).indexOf(Variable[0]) == -1) {
+            Param[Variable[0]] = Variable[1];
+        } else {
+            if(jQuery.isArray(Param[Variable[0]])) {
+                Param[Variable[0]].push(Variable[1]);
+            } else {
+                Param[Variable[0]] = [ Param[Variable[0]], Variable[1]];
+            }
+        }
+    }
+    return Param;
+}());
